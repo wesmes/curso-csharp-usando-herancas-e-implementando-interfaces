6b3076a054711985126a60c6a0a4dab11149910f
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-03-27 22:19:47 -0300

    Módulo 1 - Aula 6: Sobrecarga de método.

--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Utilitario;
 
 Funcionario pedro = new Funcionario();
 pedro.Nome = "Pedro Malazartes";
@@ -8,3 +9,18 @@ pedro.Salario = 2000.0;
 
 Console.WriteLine(pedro.Nome);
 Console.WriteLine(pedro.GetBonificacao());
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Silva";
+roberta.Cpf = "987654321";
+roberta.Salario = 5000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine("Total de bonificações: "+gerenciador.TotalDeBonificacoes);
---
+++ bytebank_ADM/Utilitario/GerenciadorDeBonificacao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytebank_ADM.Funcionarios;
+
+namespace bytebank_ADM.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+
+        public double TotalDeBonificacoes { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalDeBonificacoes += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            this.TotalDeBonificacoes += diretor.GetBonificacao();
+        }
+
+        internal void Registar(Diretor roberta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
