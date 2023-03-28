fd16bf0f445fbf7b8c4d66d3f31f973271e96b3b
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2023-03-27 21:30:51 -0300

    Módulo 1 - Aula 1: Orientação a Objetos com C#

--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using bytebank_ADM.Funcionarios;
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro Malazartes";
+pedro.Cpf = "123456789";
+pedro.Salario = 2000.0;
+
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.GetBonificacao());
